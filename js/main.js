d13c01674587379b64fac955506b8546fabaeee5
adferove <adferove@hotmail.com>
2022-01-03 22:30:14 +1100

    bubbleSorting

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ import { OrbitControls } from '../web_modules/three/examples/jsm/controls/OrbitC
 import { TGALoader } from '../web_modules/three/examples/jsm/loaders/TGALoader.js';
 
 let camera, scene, renderer, stats, cubes;
+let x1 = 5,
+  y1 = 5,
+  x2 = 0,
+  y2 = 0;
+let next = false;
+const cube_values = [0, 1, 2, 3, 4, 5];
 
 init();
 animate();
@@ -18,11 +24,11 @@ function init() {
     0.1,
     1000
   );
-  camera.position.set(0, 0, 15);
+  camera.position.set(10, 5, -10);
   //camera.position.z = 5;
 
   scene = new THREE.Scene();
-
+  scene.background = new THREE.Color(0x0000ff);
   //
 
   //const loader = new TGALoader();
@@ -34,6 +40,7 @@ function init() {
   const rows = 6;
 
   cubes = [];
+  let pos = 0;
   for (let i = 0; i < cols; i++) {
     const xRow = [];
     for (let j = 0; j < rows; j++) {
@@ -44,7 +51,11 @@ function init() {
       const cube = new THREE.Mesh(geometry, material);
       cube.position.x = i;
       cube.position.y = j;
-
+      cube.initial_pos_x = i;
+      cube.initial_pos_y = j;
+      cube.bubble_value = cellColour;
+      cube.pos = pos;
+      pos++;
       scene.add(cube);
       xRow.push(cube);
     }
@@ -89,7 +100,7 @@ function onWindowResize() {
 
 function animate() {
   requestAnimationFrame(animate);
-  move(0.01);
+  move(0.5);
   stats.update();
   render();
 }
@@ -99,10 +110,59 @@ function render() {
 }
 
 function move(speed) {
-  var d = cubes[3][3].position.x - cubes[0][0].position.x;
-  if (cubes[3][3].position.x > cubes[0][0].position.x) {
-    const val = Math.min(speed, d);
-    cubes[3][3].position.x -= val;
-    cubes[3][3].position.z -= val;
+  if (
+    cubes[x1][y1].bubble_value !== undefined &&
+    cubes[x2][y2].bubble_value !== undefined &&
+    cubes[x1][y1].bubble_value > cubes[x2][y2].bubble_value &&
+    cubes[x1][y1].pos > cubes[x2][y2].pos
+  ) {
+    next = false;
+    const dx = cubes[x1][y1].position.x - cubes[x2][y2].initial_pos_x;
+    const dy = cubes[x1][y1].position.y - cubes[x2][y2].initial_pos_y;
+    if (dx !== 0 || dy !== 0) {
+      const dz = 2 + cubes[x1][y1].position.z;
+      const valX = Math.min(speed, dx);
+      const valY = Math.min(speed, dy);
+      const valZ = Math.min(speed, dz);
+
+      cubes[x1][y1].position.z -= valZ;
+      cubes[x2][y2].position.z -= valZ;
+      if (cubes[x1][y1].position.z === -2) {
+        cubes[x1][y1].position.x -= valX;
+        cubes[x2][y2].position.x += valX;
+        cubes[x1][y1].position.y -= valY;
+        cubes[x2][y2].position.y += valY;
+      }
+    }
+
+    if (dx === 0 && dy === 0) {
+      const dzb = cubes[x1][y1].position.z * -1;
+      let valZb = Math.abs(Math.min(speed, dzb));
+      cubes[x1][y1].position.z += valZb;
+      cubes[x2][y2].position.z += valZb;
+
+      if (valZb === 0) {
+        next = true;
+        let pos1 = cubes[x1][y1].pos;
+        let pos2 = cubes[x2][y2].pos;
+        cubes[x1][y1].pos = pos2;
+        cubes[x2][y2].pos = pos1;
+        cubes[x1][y1].initial_pos_x = cubes[x1][y1].position.x;
+        cubes[x1][y1].initial_pos_y = cubes[x1][y1].position.y;
+        cubes[x2][y2].initial_pos_x = cubes[x2][y2].position.x;
+        cubes[x2][y2].initial_pos_y = cubes[x2][y2].position.y;
+      }
+    }
+    if (next) {
+      x1 = Math.floor(Math.random() * cube_values.length);
+      y1 = Math.floor(Math.random() * cube_values.length);
+      x2 = Math.floor(Math.random() * cube_values.length);
+      y2 = Math.floor(Math.random() * cube_values.length);
+    }
+  } else {
+    x1 = Math.floor(Math.random() * cube_values.length);
+    y1 = Math.floor(Math.random() * cube_values.length);
+    x2 = Math.floor(Math.random() * cube_values.length);
+    y2 = Math.floor(Math.random() * cube_values.length);
   }
 }
